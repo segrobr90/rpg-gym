cda7f9f7af01adb3525a2ec932b4954e897d7347
Matheus Costa <suehtam.segrob@gmail.com>
2026-01-29 08:43:14 -0300

    Remove prisma config files; use schema DATABASE_URL

--- apps/api/src/database/prisma.service.ts
+++ apps/api/src/database/prisma.service.ts
@@ -2,17 +2,12 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
     await this.$connect();
   }
 
-  async enableShutdownHooks() {
-    this.$on('beforeExit', async () => {
-      await this.$disconnect();
-    });
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
